54d2515605cbbb87c2c81d2874d34aa99af9db61
Viaruss <bjartoszewicz@gmail.com>
2024-02-17 10:33:09 +0100

    Initial commit

---
+++ src/main/java/com/viarus/tictoetoe/TicToeToeApplication.java
@@ -0,0 +1,13 @@
+package com.viarus.tictoetoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicToeToeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicToeToeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viarus/tictoetoe/TicToeToeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viarus.tictoetoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicToeToeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
